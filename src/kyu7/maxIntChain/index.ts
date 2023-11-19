79227c6e2c514659c7385b7618f3c973a1c8489f
Gabriel Almeida <gabriel.dev.almeida@outlook.com>
2023-11-19 01:57:37 -0300

    :recycle: Refact

--- src/kyu7/maxIntChain/index.ts
+++ src/kyu7/maxIntChain/index.ts
@@ -1,21 +1,12 @@
 export default function maxIntChain(n: number): number {
   if (n < 5) return -1;
 
-  let half = 0;
-  let biggest = 0;
-  let chain = 0;
+  const half = n / 2;
+  const isEven = n % 2 === 0;
 
-  const isOdd = n % 2 === 1;
-
-  if (isOdd) {
-    half = n / 2;
-    biggest = Math.ceil(half);
-    chain = biggest - 1;
-  } else {
-    half = n / 2;
-    biggest = half + 1;
-    chain = half - 1;
+  if (isEven) {
+    return (half + 1) * (half - 1);
   }
 
-  return biggest * chain;
+  return Math.ceil(half) * Math.floor(half);
 }
