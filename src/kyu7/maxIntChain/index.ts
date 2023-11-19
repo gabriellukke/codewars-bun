8a2fb0ab41f34a9275a3448ba52b43b471910dd5
Gabriel Almeida <gabriel.dev.almeida@outlook.com>
2023-11-19 01:43:40 -0300

    :white_check_mark: Pass tests

--- src/kyu7/maxIntChain/index.ts
+++ src/kyu7/maxIntChain/index.ts
@@ -1,3 +1,21 @@
 export default function maxIntChain(n: number): number {
-  return n;
+  if (n < 5) return -1;
+
+  let half = 0;
+  let biggest = 0;
+  let chain = 0;
+
+  const isOdd = n % 2 === 1;
+
+  if (isOdd) {
+    half = n / 2;
+    biggest = Math.ceil(half);
+    chain = biggest - 1;
+  } else {
+    half = n / 2;
+    biggest = half + 1;
+    chain = half - 1;
+  }
+
+  return biggest * chain;
 }
