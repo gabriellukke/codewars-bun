4c75d5cdfb95cb1d82ed2d3320907daf23decf50
Gabriel Almeida <gabriel.dev.almeida@outlook.com>
2023-11-19 02:13:23 -0300

    :white_check_mark: Pass tests

--- src/kyu6/index.ts
+++ src/kyu6/index.ts
@@ -1,3 +1,20 @@
 export function likes(names: string[]): string {
-  return '';
+  if (!names.length) return 'no one likes this';
+
+  const [first, second, third] = names;
+  const others = names.length - 2;
+
+  if (names.length > 3) {
+    return `${first}, ${second} and ${others} others like this`;
+  }
+
+  if (names.length === 3) {
+    return `${first}, ${second} and ${third} like this`;
+  }
+
+  if (names.length === 2) {
+    return `${first} and ${second} like this`;
+  }
+
+  return `${first} likes this`;
 }
